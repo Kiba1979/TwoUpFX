41a3205aea76373fef82ee3fa9249d097b5ac258
Adam Cooney <heeroyuy1979@gmail.com>
2023-11-02 16:31:25 +1030

    Working update

--- src/main/java/com/kiba/twoupfx/SignUpController.java
+++ src/main/java/com/kiba/twoupfx/SignUpController.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,7 +16,7 @@ public class SignUpController implements Initializable {
     @FXML
     private Button back;
     @FXML
-    private TextField playerName;
+    private TextField username;
     @FXML
     private PasswordField password;
     @FXML
@@ -29,17 +30,21 @@ public class SignUpController implements Initializable {
         back.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                DBUtils.changeScene(event, "two-up.fxml", null, null);
+                DBUtils.changeScene(handleBackButton(event), "two-up.fxml", null);
             }
         });
 
         signup.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                DBUtils.signUpUser(event, "game-time.fxml", playerName.getText(), password.getText());
+                DBUtils.signUpUser(event, "game-time.fxml", username.getText(), password.getText());
             }
         });
     }
 
+    @FXML
+    public ActionEvent handleBackButton (ActionEvent event) {
+        return event;
+    }
 
 }
